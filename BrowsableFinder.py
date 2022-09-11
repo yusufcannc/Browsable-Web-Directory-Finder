c716ee1f823ceb5a862c0c44079b9b08b4b8f2a9
Yusuf Can <67974607+yusufcannc@users.noreply.github.com>
2022-09-11 22:29:44 +0300

    Update BrowsableFinder.py

--- BrowsableFinder.py
+++ BrowsableFinder.py
@@ -8,6 +8,7 @@ from bs4 import BeautifulSoup
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+
 class AllGetLink:
     def __init__(self):
         self.paths = []
@@ -17,11 +18,12 @@ class AllGetLink:
         self.filename = parser.parse_args()
         self.getURLS()
         self.getReq()
+        
     def getURLS(self):
         with open(self.filename.file, "r+") as file:
             for i in file:
                 self.urls.append(i.split("\n")[0])
-                
+
     def getReq(self):
         for url in self.urls:
                 try:
@@ -31,15 +33,15 @@ class AllGetLink:
                     )  # The reason for separating domains is to search only for this domain in links and scripts.
                     req = requests.get(url, timeout=5, verify=False)
                     soup = BeautifulSoup(req.content, "html.parser")
-                    redirectFind = soup.find("meta", attrs={"http-equiv":"refresh"})
+                    redirectFind = soup.find("meta", attrs={"http-equiv": "refresh"})
                     redirect_re = re.compile('<meta[^>]*?url=(.*?)["\']', re.IGNORECASE)
                     match = redirect_re.search(str(redirectFind))
                     try:
                         if match:
-                            new_url =  url + "/" + match.groups()[0].strip()
+                            new_url = url + "/" + match.groups()[0].strip()
                             req_refresh = requests.get(new_url, timeout=5, verify=False)
                             soup_refresh = BeautifulSoup(req_refresh.content, "html.parser")
-                            if "Index of" in str(soup_refresh.title)  or  "[PARENTDIR]" in str(soup_refresh):
+                            if "Index of" in str(soup_refresh.title) or "[PARENTDIR]" in str(soup_refresh):
                                 print("Found Index of " + new_url)
                             else:
                                 self.getCSSLinks(soup_refresh)
@@ -69,7 +71,7 @@ class AllGetLink:
                     
     def getCSSLinks(self, url):
         for link in url.find_all("link"):
-            if link.get("href") != None:
+            if link.get("href") not None:
                 if "http" in link.get("href"):
                     firstPath = link.get("href").split("/")
                     if firstPath in self.paths:
@@ -78,7 +80,7 @@ class AllGetLink:
                         res = "/"
                         a = 0
                         for i in range(3, len(firstPath) - 1):
-                            for j in firstPath[3 + a : i]:
+                            for j in firstPath[3 + a: i]:
                                 res = res + j + "/"
                                 a += 1
                             if res in self.paths:
@@ -93,7 +95,7 @@ class AllGetLink:
                         res = "/"
                         a = 0
                         for i in range(0, len(firstPath) - 1):
-                            for j in firstPath[0 + a : i]:
+                            for j in firstPath[0 + a: i]:
                                 res = res + j + "/"
                                 a += 1
                             if res in self.paths:
@@ -103,7 +105,7 @@ class AllGetLink:
 
     def getJSlinks(self, url):
         for link in url.find_all("script"):
-            if link.get("src") != None:
+            if link.get("src") not None:
                 if "http" in link.get("src"):
                     firstPath = link.get("src").split("/")
                     if firstPath in self.paths:
@@ -112,8 +114,7 @@ class AllGetLink:
                         res = "/"
                         a = 0
                         for i in range(3, len(firstPath) - 1):
-
-                            for j in firstPath[3 + a : i]:
+                            for j in firstPath[3 + a: i]:
                                 res = res + j + "/"
                                 a += 1
                             if res in self.paths:
@@ -128,7 +129,7 @@ class AllGetLink:
                         res = "/"
                         a = 0
                         for i in range(0, len(firstPath) - 1):
-                            for j in firstPath[0 + a : i]:
+                            for j in firstPath[0 + a: i]:
                                 res = res + j + "/"
                                 a += 1
                             if res in self.paths:
@@ -138,7 +139,7 @@ class AllGetLink:
 
     def getImg(self, url):
         for link in url.find_all("img"):
-            if link.get("src") != None:
+            if link.get("src") not None:
                 if "http" in link.get("src"):
                     firstPath = link.get("src").split("/")
                     if firstPath in self.paths:
@@ -147,7 +148,7 @@ class AllGetLink:
                         res = "/"
                         a = 0
                         for i in range(3, len(firstPath) - 1):
-                            for j in firstPath[3 + a : i]:
+                            for j in firstPath[3 + a: i]:
                                 res = res + j + "/"
                                 a += 1
                             if res in self.paths:
@@ -164,7 +165,7 @@ class AllGetLink:
                         res = "/"
                         a = 0
                         for i in range(0, len(firstPath) - 1):
-                            for j in firstPath[0 + a : i]:
+                            for j in firstPath[0 + a: i]:
                                 res = res + j + "/"
                                 a += 1
                             if res in self.paths:
@@ -176,7 +177,7 @@ class AllGetLink:
         for i in self.paths:
             req = requests.get(url + i, verify=False)
             html = BeautifulSoup(req.text, "html.parser")
-            if  "Index of" in str(html.title) or  "[PARENTDIR]" in str(html):
+            if "Index of" in str(html.title) or "[PARENTDIR]" in str(html):
                 print("Found Index of " + url + i)
             else:
                 pass
